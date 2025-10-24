a8d73831bec95e0c6f3aa536ad227534412878ec
Ivan <ideshchenko@yandex.ru>
2025-10-24 21:02:13 +0300

    feat: add Auth page using qt desginer

---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+import pymysql as pms
+from pymysql import cursors
+from AuthMain import Ui_MainWindow
+from Database import Database
+from PyQt6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+
+
+if __name__=="__main__":
+    app=QApplication(sys.argv)
+    window=MainWindow()
+    window.show()
+    sys.exit(app.exec())
+
